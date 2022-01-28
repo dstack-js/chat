8c35c438e7c89ed7e9ccb7a023efa959629f3569
Mikhailo Marynenko <0x77dev@protonmail.com>
2022-01-28 03:59:38 +0200

    feat(chat): PoC

---
+++ README.md
@@ -0,0 +1,32 @@
+peerchat
+===============
+
+Peer-to-peer terminal chat based on [DStack](https://github.com/dstack-js/dstack)
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/peerchat.svg)](https://npmjs.org/package/peerchat)
+[![Downloads/week](https://img.shields.io/npm/dw/peerchat.svg)](https://npmjs.org/package/peerchat)
+[![License](https://img.shields.io/npm/l/peerchat.svg)](https://github.com/dstack-js/chat/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g peerchat
+$ peerchat COMMAND
+running command...
+$ peerchat (-v|--version|version)
+peerchat/0.0.0 darwin-x64 node-v16.13.1
+$ peerchat --help [COMMAND]
+USAGE
+  $ peerchat COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/chat.ts
@@ -0,0 +1,169 @@
+/* eslint-disable node/no-extraneous-import */
+/* eslint-disable no-process-exit */
+/* eslint-disable unicorn/no-process-exit */
+import {create} from '@dstack-js/ipfs'
+import {Stack} from '@dstack-js/lib'
+import {PubSub} from '@dstack-js/lib/src/pubsub'
+const wrtc = require('wrtc')
+import * as blessed from 'blessed'
+
+interface Message {
+  nickname?: string;
+  message: string;
+}
+
+export const run = async (room: string, nickname?: string) => {
+  const ipfs = await create({
+    repo: process.env.IPFS_REPO,
+    relay: {
+      enabled: true, // enable relay dialer/listener (STOP)
+      hop: {
+        enabled: true, // make this node a relay (HOP)
+      },
+    },
+    config: {
+      Addresses: {
+        Swarm: ['/ip4/0.0.0.0/tcp/0', '/dns4/dstack-relay.herokuapp.com/tcp/443/wss/p2p-webrtc-star'],
+      },
+      Discovery: {
+        MDNS: {
+          Enabled: true,
+          Interval: 1,
+        },
+        webRTCStar: {
+          Enabled: true,
+        },
+      },
+      Bootstrap: ['/dns4/dstack-relay.herokuapp.com/tcp/443/wss/p2p-webrtc-star/p2p/QmV2uXBKbii29iJKHKVy8sx5m49qdDTBYNybVoa5uLJtrf'],
+    },
+  }, wrtc)
+
+  const stack = await Stack.create('dstack-chat', ipfs)
+  const pubsub = stack.pubsub as PubSub<Message>
+
+  const screen = blessed.screen({
+    smartCSR: true,
+    title: `#${room}`,
+  })
+
+  const messageList = blessed.list({
+    align: 'left',
+    mouse: true,
+    keys: true,
+    width: '100%',
+    height: '90%',
+    border: 'line',
+    top: 0,
+    left: 0,
+    items: [
+      'dstack: connected',
+      `dstack: your id is ${stack.id.slice(-5)}`,
+      'dstack: use /help to see commands',
+    ],
+  })
+
+  // Append our box to the screen.
+  const input = blessed.textarea({
+    bottom: 0,
+    height: '10%',
+    inputOnFocus: true,
+    clickable: true,
+    label: ` ID: ${stack.id.slice(-5)} `,
+    padding: {
+      top: 1,
+      left: 2,
+    },
+    style: {
+      fg: '#787878',
+      bg: '#454545',
+
+      focus: {
+        fg: '#f6f6f6',
+        bg: '#353535',
+      },
+    },
+  })
+
+  input.key('enter', async function () {
+    const message = input.getValue()
+    if (message.length === 0) return
+
+    if (message.startsWith('/connect ')) {
+      await stack.ipfs.swarm.connect(message.split('/connect ')[1])
+      input.clearValue()
+      screen.render()
+      return
+    }
+
+    if (message.startsWith('/addr')) {
+      const {addresses} = await ipfs.id()
+
+      for (const addr of addresses) {
+        messageList.addItem(`dstack: ${addr.toString()}`)
+      }
+
+      input.clearValue()
+      screen.render()
+      return
+    }
+
+    if (message.startsWith('/help')) {
+      messageList.addItem('dstack: /addr - your addresses')
+      messageList.addItem('dstack: /connect <addr> - manually connect to peer')
+      messageList.addItem('dstack: /peers - peers list')
+      input.clearValue()
+      screen.render()
+      return
+    }
+
+    if (message.startsWith('/peers')) {
+      const peers = await stack.peers()
+
+      for (const peer of peers) {
+        messageList.addItem(`dstack: ${peer.id}`)
+      }
+
+      input.clearValue()
+      screen.render()
+      return
+    }
+
+    try {
+      await pubsub.publish('chat', {nickname, message})
+    } catch {
+      // error handling
+    } finally {
+      input.clearValue()
+      screen.render()
+    }
+  })
+
+  screen.key(['escape', 'q', 'C-c'], function () {
+    return process.exit(0)
+  })
+
+  screen.append(messageList)
+  screen.append(input)
+  input.focus()
+
+  await pubsub.subscribe('chat', event => {
+    messageList.addItem(`${event.data.nickname ? `${event.data.nickname} (${event.from.slice(-5)})` : event.from.slice(-5)}: ${event.data.message}`)
+    messageList.scrollTo(100)
+    screen.render()
+  })
+
+  stack.onPeerConnect(async peer => {
+    messageList.setLabel(` Messages #${room} - Peers: ${await pubsub.peers('chat')} `)
+    messageList.addItem(`dstack: peer connected ${peer.id.slice(-5)}`)
+    messageList.scrollTo(100)
+    screen.render()
+  })
+
+  setInterval(async () => {
+    messageList.setLabel(` Messages #${room} - Peers: ${await pubsub.peers('chat')} `)
+    screen.render()
+  }, 1000)
+
+  messageList.scrollTo(100)
+  screen.render()
+}
